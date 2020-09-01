588416d7ef16ef4d5ab84c88c1f10fc7687031b5
Rowen Atkinson <rowen.atkinson@gmail.com>
2020-09-01 17:19:55 +1000

    FEAT: update to make optionsetfield toggle alternate field display

--- js/AlternateFormField.js
+++ js/AlternateFormField.js
@@ -8,28 +8,61 @@ so.alternatefield = {
     },
     alternatefieldsetup: function () {
 
-        $('.selected-field').each(function () {
-            $(this).on('change', function() {
+        $('select.selected-field').each(function () {
+            $(this).on('change', function () {
                 so.alternatefield.showHideAlternateValue(this);
             });
             so.alternatefield.showHideAlternateValue(this);
+        });
+        $('.selected-field input').each(function () {
+            $(this).on('change', function () {
+                so.alternatefield.showHideAlternateValueCheckbox(this);
+            });
+            so.alternatefield.showHideAlternateValueCheckbox(this);
         });
 
     },
     showHideAlternateValue: function (el) {
         var alternatename = $(el).data('for');
         // console.log($(el).val());
-        var alternate = $('[data-fieldid='+alternatename+']');
+        var alternate = $('[data-fieldid=' + alternatename + ']');
         var alternatediv = $(alternate).closest('.AlternateFormFieldAlternateValue');
 
         var val = $(el).val();
         if (val == '') {
-            $('[data-for='+alternatename+']'+' :checked').each(function () {
+            $('[data-for=' + alternatename + ']' + ' :checked').each(function () {
                 val += $(this).val();
             });
         }
 
         if (val && val.indexOf('Other') !== -1) {
+            $(alternate).show();
+            $(alternatediv).show();
+        } else {
+            $(alternate).hide();
+            $(alternatediv).hide();
+        }
+
+    },
+    showHideAlternateValueCheckbox: function (el) {
+        var alternate = $('[data-fieldid=' + alternatename + ']');
+
+        var alternatename = $(el).data('for');
+        var alternate = $(el).parents('.AlternateFormField').find('.AlternateFormFieldAlternateValue input');
+        var alternatediv = $(alternate).closest('.AlternateFormFieldAlternateValue');
+        var val = false;
+
+        if ($(el).is(':checked')) {
+            var val = $(el).val();
+        }
+
+        if (val == '') {
+            $('[data-for=' + alternatename + ']' + ' :checked').each(function () {
+                val += $(this).val();
+            });
+        }
+
+        if (val && val === "Other" && $(el).is(':checked')) {
             $(alternate).show();
             $(alternatediv).show();
         } else {
